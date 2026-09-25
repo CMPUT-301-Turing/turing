d747419397843b3089ea1b3bdb77e664f116b922
btipan <btipan@ualberta.ca>
2026-09-24 19:27:13 -0600

    conflict resolved

--- app/src/main/java/com/example/turing/Shape.kt
+++ app/src/main/java/com/example/turing/Shape.kt
@@ -1,4 +1,4 @@
 package com.example.turing
 
-abstract class Shape (var x: Int, var y: Int, val color: String = "blue") {
+abstract class Shape (var x: Int, var y: Int, val color: String = "Blue") {
 }
